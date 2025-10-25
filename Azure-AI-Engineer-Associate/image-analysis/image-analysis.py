86e7ef3e6fe7161aa28fbbcaae06d83b442d87df
Carlos Roberto Flores Luna <carlos.luna+oxxogh@valtre.com>
2025-10-25 14:02:32 -0600

    feat : Tutorial analyze images

--- Azure-AI-Engineer-Associate/image-analysis/image-analysis.py
+++ Azure-AI-Engineer-Associate/image-analysis/image-analysis.py
@@ -11,6 +11,7 @@ from azure.ai.vision.imageanalysis import ImageAnalysisClient
 from azure.ai.vision.imageanalysis.models import VisualFeatures
 from azure.core.credentials import AzureKeyCredential
 
+
 def main():
 
     # Clear the console
