211ea9a238604553a818e7d5c1299fe42ce2d645
corderodedios182 <carlos.luna+oxxogh@valtre.com>
2025-10-25 14:38:17 -0600

    test

---
+++ Azure Developer Certificate/import pandas as pd.py
@@ -0,0 +1,4 @@
+# import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
