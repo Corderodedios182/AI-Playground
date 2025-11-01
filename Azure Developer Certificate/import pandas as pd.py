a668284eebca911b5f1a4ebc157c30de874ffe4e
corderodedios182 <carlos.luna+oxxogh@valtre.com>
2025-10-31 21:08:31 -0600

    feat : test

--- Azure Developer Certificate/import pandas as pd.py
+++ Azure Developer Certificate/import pandas as pd.py
@@ -2,3 +2,5 @@
 from azure.ai.vision.imageanalysis import ImageAnalysisClient
 from azure.ai.vision.imageanalysis.models import VisualFeatures
 from azure.core.credentials import AzureKeyCredential
+
+#NEW
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is my zone security, for breack things.
 
 ![Ejemplo](Solarpunk.jpg)
+
