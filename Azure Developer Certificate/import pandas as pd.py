7c81a62e85629b544499cb53dbfcb1a167c868fd
corderodedios182 <corderodedios182@gmail.com>
2025-10-31 21:31:05 -0600

    f

--- Azure Developer Certificate/import pandas as pd.py
+++
@@ -1,6 +0,0 @@
-# import namespaces
-from azure.ai.vision.imageanalysis import ImageAnalysisClient
-from azure.ai.vision.imageanalysis.models import VisualFeatures
-from azure.core.credentials import AzureKeyCredential
-
-#NEW
